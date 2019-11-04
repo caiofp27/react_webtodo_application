659f5463a1fb51979600f68c3a579baddb056ee0
Caio <caioymc@live.com>
2019-11-04 20:03:39 +0000

    Updates

---
+++ precache-manifest.4dea9c29421ebdd0510887c32fa0731b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821d53b8d68ed38a7d212bfddce0f4d2",
+    "url": "/react_webtodo_application/index.html"
+  },
+  {
+    "revision": "e5bd470783fc8fbc93ae",
+    "url": "/react_webtodo_application/static/css/main.c0c05165.chunk.css"
+  },
+  {
+    "revision": "815c61ab12fc26f54ca8",
+    "url": "/react_webtodo_application/static/js/2.be22d6eb.chunk.js"
+  },
+  {
+    "revision": "e5bd470783fc8fbc93ae",
+    "url": "/react_webtodo_application/static/js/main.d0f60c8d.chunk.js"
+  },
+  {
+    "revision": "e32903c6e6f15b460831",
+    "url": "/react_webtodo_application/static/js/runtime-main.bffcee1b.js"
+  }
+]);
